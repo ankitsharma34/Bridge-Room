1330f355eabc3afa2689a3823bdd8d76ce7f377a
ankitsharma34 <ankitsharma04.iiits@gmail.com>
2026-05-25 23:12:58 +0530

    Prisma setup done

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,6 +1,7 @@
 import app from "./app.js";
+import { env } from "./config/env.js";
 
-const PORT = process.env.PORT || 5000;
+const PORT = env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`BridgeRoom Server running on http://localhost:${PORT}`);
